e0cfc7115f5550b44acbd8edae08627aa604886a
hooray <hrfudan@gmail.com>
2019-06-17 11:52:42 +0800

    added units in timer

--- kernel/timer/timer.cpp
+++ kernel/timer/timer.cpp
@@ -6,19 +6,29 @@ namespace tmr {
   inline auto now() {
     return high_resolution_clock::now();
   }
+
+  template < typename T >
+  constexpr auto convert( const T& reading_in_ns, std::string unit, std::string unit_fallback ) noexcept {
+    if ( "ms" == unit ) return reading_in_ns / 1e6;
+    if ( "us" == unit ) return reading_in_ns / 1e3;
+    if ( "ns" == unit ) return reading_in_ns;
+    if ( "s" == unit ) return  reading_in_ns/ 1e9;
+    return convert( reading_in_ns, unit_fallback, "ms"); // return default unit if unspecified unit is passed in
+  }
+
   using timepoint_t = decltype(now());
 
   Timestamp::Timestamp()
     : _t( now() ) {}
 
-  double Timestamp::lapse() {
+  double Timestamp::lapse(std::string unit) const {
     auto dur = duration_cast<nanoseconds>( now() - std::any_cast<timepoint_t>(_t) );
-    return static_cast<double>( dur.count() ) / 1e6;
+    return convert( static_cast<double>( dur.count() ), unit, _unit );
   }
 
   double Timestamp::operator- ( const Timestamp& other ) {
     auto dur = duration_cast<nanoseconds>( std::any_cast<timepoint_t>(_t) - std::any_cast<timepoint_t>(other._t) );
-    return static_cast<double>( dur.count() ) / 1e6;
+    return convert( static_cast<double>( dur.count() ), _unit, "ms" );
   }
 
 }
--- kernel/timer/timer.hpp
+++ kernel/timer/timer.hpp
@@ -1,14 +1,25 @@
 #ifndef _TIMER_HPP_
 #define _TIMER_HPP_
 #include <any>
+#include <string>
 
 namespace tmr {
   struct Timestamp {
   private:
     std::any _t;
+    std::string _unit{"ms"}; // possible values are s, ms, us, ns
   public:
     Timestamp();
-    double lapse();
+
+    double lapse(std::string unit) const;
+    inline double lapse() const { return lapse(_unit); }
+
+    inline void set_unit(std::string unit) noexcept {
+      _unit = std::move(unit);
+      if ( _unit != "ms" && _unit != "us" && _unit != "ns" && _unit != "s" ) _unit = "ms";
+    }
+    inline const std::string& unit() const noexcept { return _unit; }
+
     double operator- ( const Timestamp& other );
   };
 
