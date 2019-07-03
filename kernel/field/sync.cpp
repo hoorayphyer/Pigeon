5fef814b5e9d2c6739ffdb0bf04e548fdb18fc6d
hooray <hrfudan@gmail.com>
2019-07-03 13:12:26 -0400

    a brutal force fix to duplicate instantiation in sync.cpp

--- kernel/field/sync.cpp
+++ kernel/field/sync.cpp
@@ -3,26 +3,32 @@
 
 using namespace pic;
 
-// TODO what if real_t = real_j_t or real_export_t
+// TODO If real_t = real_j_t or real_export_t, there will be duplicate instantiation. For now we use just use explicit types
 namespace field {
   template
-  void copy_sync_guard_cells( Field<real_t, 1, DGrid>& field, const mpi::CartComm& comm );
+  void copy_sync_guard_cells( Field<float, 1, DGrid>& , const mpi::CartComm& );
+  template
+  void copy_sync_guard_cells( Field<float, 3, DGrid>& , const mpi::CartComm& );
+  template
+  void merge_sync_guard_cells( Field<float, 3, DGrid>& , const mpi::CartComm& );
+  template
+  void merge_sync_guard_cells( Field<float, 1, DGrid>& , const mpi::CartComm& );
 
   template
-  void copy_sync_guard_cells( Field<real_t, 3, DGrid>& field, const mpi::CartComm& comm );
-
-  //template
-  //void copy_sync_guard_cells( Field<real_j_t, 3, DGrid>& field, const mpi::CartComm& comm );
+  void copy_sync_guard_cells( Field<double, 1, DGrid>& , const mpi::CartComm& );
+  template
+  void copy_sync_guard_cells( Field<double, 3, DGrid>& , const mpi::CartComm& );
+  template
+  void merge_sync_guard_cells( Field<double, 3, DGrid>& , const mpi::CartComm& );
+  template
+  void merge_sync_guard_cells( Field<double, 1, DGrid>& , const mpi::CartComm& );
 
   template
-  void merge_sync_guard_cells( Field<real_j_t, 3, DGrid>& field, const mpi::CartComm& comm );
-
+  void copy_sync_guard_cells( Field<long double, 1, DGrid>& , const mpi::CartComm& );
   template
-  void merge_sync_guard_cells( Field<real_t, 1, DGrid>& field, const mpi::CartComm& comm );
-
+  void copy_sync_guard_cells( Field<long double, 3, DGrid>& , const mpi::CartComm& );
+  template
+  void merge_sync_guard_cells( Field<long double, 3, DGrid>& , const mpi::CartComm& );
+  template
+  void merge_sync_guard_cells( Field<long double, 1, DGrid>& , const mpi::CartComm& );
 }
-
-//namespace field {
-//  template
-//  void copy_sync_guard_cells( Field<real_export_t, 1, DGrid>& field, const mpi::CartComm& comm );
-//}
