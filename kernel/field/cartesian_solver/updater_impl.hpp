4ba636e64dbacb7ad386e7c21dfa9be85ffe2125
hooray <hrfudan@gmail.com>
2019-08-16 17:19:00 -0400

    finished cartesian solver

--- kernel/field/cartesian_solver/updater_impl.hpp
+++ kernel/field/cartesian_solver/updater_impl.hpp
@@ -50,34 +50,14 @@ namespace field {
 
   };
 
-  template < int DGrid, typename Real, typename RealJ >
-  struct RHS {
-    const Mesh<DGrid>& mesh;
-    const Field<RealJ,3,DGrid>& J;
-    const Derivative<DGrid,Real>& d;
-
-    template < int I_ >
-    inline void ofB( Field<Real,3,DGrid>& B_new, Real dt, Real alpha, Real preJ, const Field<Real,3,DGrid>& E, const Field<Real,3,DGrid>& B ) const {
-      constexpr int J_ = (I_+1)%3;
-      constexpr int K_ = (I_+2)%3;
-      for ( const auto& I : apt::Block(mesh.bulk_dims()) ) {
-        int idx = mesh.linearized_index_of_whole_mesh(I);
-        B_new[I_][idx] = B[I_][idx] + dt*dt*alpha*(1.0-alpha) * d.Lapl(B[I_][idx])
-          - dt * ( d.template Curl<I_>(E[K_][idx - d.s[J_]], E[J_][idx - d.s[K_]] ) + alpha*dt*preJ* d.template Curl<I_>(J[K_][idx - d.s[J_]], J[J_][idx - d.s[K_]] ));
-      }
+  template < int I, int DGrid, typename Real, typename Real2 >
+  inline void add_plus_curl( Field<Real,3,DGrid>& f, Real pre, const Field<Real2,3,DGrid>& f2, const Derivative<DGrid,Real>& d ) {
+    constexpr int J = (I+1)%3;
+    constexpr int K = (I+2)%3;
+    for ( int i = 0; i < f[0].data().size(); ++i ) {
+      f[I][i] += pre * d.template Curl<I>( f2[K][i], f2[J][i] );
     }
-
-    template < int I_ >
-    inline void ofE( Field<Real,3,DGrid>& E, Real dt, Real alpha, Real preJ, const Field<Real,3,DGrid>& B_old, const Field<Real,3,DGrid>& B_new ) const {
-      constexpr int J_ = (I_+1)%3;
-      constexpr int K_ = (I_+2)%3;
-      for ( const auto& I : apt::Block(mesh.bulk_dims()) ) {
-        int idx = mesh.linearized_index_of_whole_mesh(I);
-        E[I_][idx] += dt * ( alpha * d.template Curl<I_>( B_new[K_][idx], B_new[J_][idx] ) + ( 1.0 - alpha ) * d.template Curl<I_>( B_old[K_][idx], B_old[J_][idx] ) - preJ * J[I_][idx] );
-      }
-    }
-
-  };
+  }
 
 
   template < typename Real, int DGrid, typename RealJ >
@@ -90,6 +70,7 @@ namespace field {
                   Real dt,
                   Real preJ
                   ) const {
+    // NOTE E,B,J are assumed to have been synced
     constexpr Real alpha = 0.5;
 
     const auto& mesh = E.mesh(); // TODO this implicitly requires J to have same grid as E
@@ -97,30 +78,51 @@ namespace field {
     for ( int i = 0; i < DGrid; ++i ) strd[i] = mesh.stride(i);
     Derivative<DGrid,Real> d{strd,grid};
 
-    Field<Real,3,DGrid> tmp(mesh);
-
-    RHS<DGrid,Real,RealJ> rhs{mesh,J,d};
+    Field<Real,3,DGrid> B_old(mesh);
+    std::swap(B,B_old);
 
     // update B
-    rhs.template ofB<0>(tmp,dt,alpha,preJ,E,B);
-    rhs.template ofB<1>(tmp,dt,alpha,preJ,E,B);
-    rhs.template ofB<2>(tmp,dt,alpha,preJ,E,B);
+    for ( int C = 0; C < 3; ++C ) {
+      Real abtt = dt*dt*alpha*(1.0-alpha);
+      for ( int i = 0; i < B[0].data().size(); ++i ) {
+        B[C][i] = B[C][i] + abtt * d.Lapl(B[C][i]);
+      }
+    }
+    add_plus_curl<0>(B,-dt,E,d);
+    add_plus_curl<1>(B,-dt,E,d);
+    add_plus_curl<2>(B,-dt,E,d);
 
-    copy_sync_guard_cells(tmp, comm);
+    add_plus_curl<0>(B,-alpha*dt*dt*preJ,J,d);
+    add_plus_curl<1>(B,-alpha*dt*dt*preJ,J,d);
+    add_plus_curl<2>(B,-alpha*dt*dt*preJ,J,d);
 
-    // TODO solve a sparse matrix
+    copy_sync_guard_cells(B, comm);
+
+    // update E partly before B_old is used and modified
+    add_plus_curl<0>(E,(Real(1.0) - alpha) * dt,B_old,d);
+    add_plus_curl<1>(E,(Real(1.0) - alpha) * dt,B_old,d);
+    add_plus_curl<2>(E,(Real(1.0) - alpha) * dt,B_old,d);
+
     int num_iteration = 5;
     for ( int n = 0; n < num_iteration; ++n ) {
-      
-      // copy_sync_guard_cells(tmp, comm);
+      std::swap(B,B_old);
+      Real aa = alpha * alpha * dt * dt;
+      for ( int C = 0; C < 3; ++C ) {
+        for ( int i = 0; i < B[0].data().size(); ++i ) {
+          B[C][i] = B_old[C][i] + aa * d.Lapl(B_old[C][i]);
+        }
+      }
+      copy_sync_guard_cells(B, comm);
     }
 
-    std::swap(B,tmp);
-
-    // update E
-    rhs.template ofE<0>(E,dt,alpha,preJ,tmp,B);
-    rhs.template ofE<1>(E,dt,alpha,preJ,tmp,B);
-    rhs.template ofE<2>(E,dt,alpha,preJ,tmp,B);
+    // update E, the rest of it
+    add_plus_curl<0>(E,alpha*dt,B,d);
+    add_plus_curl<1>(E,alpha*dt,B,d);
+    add_plus_curl<2>(E,alpha*dt,B,d);
+    for ( int C = 0; C < 3; ++C ) {
+      for( int i = 0; i < E[0].data().size(); ++i )
+        E[C][i] -= preJ * dt * J[C][i];
+    }
 
     copy_sync_guard_cells(E, comm);
   }
