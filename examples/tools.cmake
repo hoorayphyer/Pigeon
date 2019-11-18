95cedd21ed84d082fa3c6b9b1ff6f5f115705d46
cuijing1991 <cuijing1991@gmail.com>
2019-11-18 15:29:25 -0500

    tools.cmake content not right

--- examples/tools.cmake
+++ examples/tools.cmake
@@ -1,2 +1,2 @@
-set(CMAKE_C_COMPILER "$ENV{HOME}/Yewa/.brew/bin/gcc-9")
-set(CMAKE_CXX_COMPILER "$ENV{HOME}/Yewa/.brew/bin/g++-9")
+set(CMAKE_C_COMPILER "/usr/local/bin/gcc-9")
+set(CMAKE_CXX_COMPILER "/usr/local/bin/g++-9")
