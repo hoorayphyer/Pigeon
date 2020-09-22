2d6e0cfebac926105583dbd2825218c91c57c4a2
hooray <hrfudan@gmail.com>
2020-09-21 21:36:05 -0400

    retired haugbolle solver

--- kernel/field/haugbolle_solver/updater.cpp
+++
@@ -1,7 +0,0 @@
-#include "field/haugbolle_solver/updater_impl.hpp"
-#include "pic.hpp"
-
-namespace field {
-  template struct Haugbolle<pic::real_t, pic::DGrid, pic::real_j_t>;
-  template struct HaugbolleBdry<pic::real_t, pic::DGrid, pic::real_j_t>;
-}
